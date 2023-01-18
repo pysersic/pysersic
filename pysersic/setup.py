99fc36ac203fff5c7587e2fb488fb97f11ea3b4d
Imad Pasha <ipasha@berkeley.edu>
2023-01-18 14:17:20 -0500

    setup.py

---
+++ pysersic/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="pysersic",
+    version="0.1",
+    author="Imad Pasha & Tim Miller",
+    author_email="imad.pasha@yale.edu",
+    description="A Tool for fitting sersic profiles in python",
+    packages=["pysersic"]
+)
