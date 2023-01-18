1d1a8d26fd70e8ea9074346236615fe34adc60ea
Imad Pasha <ipasha@berkeley.edu>
2023-01-18 16:53:39 -0500

    use max pix instead of center

--- pysersic/utils/utils.py
+++ pysersic/utils/utils.py
@@ -17,12 +17,13 @@ def autoprior(image,verbose=False):
     ellip_prior = dist.Uniform(0,1)
     theta_prior = dist.Uniform(0,jnp.pi)
     log_n_prior = dist.Uniform(0,1)
-    x_cen = im_shape[1] / 2.0 
-    y_cen = im_shape[0] / 2.0
-    x_third = im_shape[1] / 3.0
-    y_third = im_shape[0] / 3.0
-    x0_prior = dist.Uniform(x_cen-x_third,x_cen+x_third)
-    y0_prior = dist.Uniform(y_cen-y_third,y_cen+y_third)
+    max_pix = jnp.argmax(image)
+    unraveled = jnp.unravel_index(max_pix,image.shape)
+    x0_max = unraveled[1]
+    y0_max = unraveled[0]
+
+    x0_prior = dist.Uniform(x0_max-10,x0_max+10)
+    y0_prior = dist.Uniform(y0_max-10,y0_max+10)
 
     prior_dict = {
         'log_flux': log_flux_prior,
@@ -38,8 +39,8 @@ def autoprior(image,verbose=False):
             f'log_flux: {jnp.floor(jnp.log10(image_sum))} - {jnp.ceil(jnp.log10(image_sum))}'
         )
         print(f'reff: 0.1 - {image_dim_min}')
-        print(f'x0: {x_cen-x_third} - {x_cen+x_third}')
-        print(f'y0: {y_cen-y_third} - {y_cen+y_third}')
+        print(f'x0: {x0_max-10} - {x0_max+10}')
+        print(f'y0: {y0_max-10} - {y0_max+10}')
 
     return prior_dict 
 
