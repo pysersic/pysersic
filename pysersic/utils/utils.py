2db5a28b473bc65949f67657795ed5b4cb09ad84
Imad Pasha <ipasha@berkeley.edu>
2023-01-19 11:40:08 -0500

    batch priors for list of sources

--- pysersic/utils/utils.py
+++ pysersic/utils/utils.py
@@ -1,5 +1,5 @@
 
-from numpyro import distributions as dist
+from numpyro import distributions as dist, infer
 import jax.numpy as jnp 
 
 
@@ -43,4 +43,88 @@ def autoprior(image,verbose=False):
         print(f'y0: {y0_max-10} - {y0_max+10}')
 
     return prior_dict 
+
+
+
+class BatchPriors():
+    def __init__(self,catalog):
+        """
+        Ingest a catalog-like data structure containing prior positions and parameters for multiple galaxies in a single image.
+        The format of the catalog can be a `pandas.DataFrame`, `numpy` RecordArray, dictionary, or any other format so-long as 
+        the following fields exist and can be directly indexed: 'X', 'Y', 'LOGFLUX', and 'R'. Optional additional fields include
+        'THETA' and 'ELLIP' -- if these are on hand they may be provided. If only some galaxies have these measured, the flag -99
+        can be used to indicate as such. All columns/fields must be the same length
+        """
+        self.catalog = catalog 
+        self.pos_sigma = 5.0
+        self.rpix_sigma = 5.0 
+        self.ellip_sigma = 0.2
+        self.theta_sigma = jnp.pi/2.0
+        self.logflux_sigma = 0.5
+
+        self.parse_catalog()         
+
+        
+
+    def parse_catalog(self):
+        self.prior_dict = {} 
+        try:
+            theta_exists = self.catalog['THETA']
+            theta_exists = True
+        except KeyError:
+            theta_exists=False
+        try:
+            ellip_exists = self.catalog['ELLIP']
+            ellip_exists = True
+        except KeyError:
+            ellip_exists = False
+        if theta_exists and ellip_exists:
+            for i in range(len(self.catalog)):
+                self.prior_dict[f'x0_{i}'] = dist.Normal(self.catalog['X'][i],self.pos_sigma)
+                self.prior_dict[f'y0_{i}'] = dist.Normal(self.catalog['Y'][i],self.pos_sigma)
+                self.prior_dict[f'log_flux_{i}'] = dist.Normal(self.catalog['LOGFLUX'][i],self.logflux_sigma)
+                self.prior_dict[f'r_eff_{i}'] = dist.TruncatedNormal(self.catalog['R'][i], self.rpix_sigma,low=1) 
+                if self.catalog['THETA'][i] != -99.0:
+                    self.prior_dict[f'theta_{i}'] = dist.Normal(self.catalog['THETA'][i],self.theta_sigma)
+                else:
+                    self.prior_dict[f'theta_{i}'] = dist.Uniform(0,jnp.pi)
+                if self.catalog['ELLIP'][i] != -99.0:
+                    self.prior_dict[f'ellip_{i}'] = dist.TruncatedNormal(self.catalog['ELLIP'][i],self.ellip_sigma,low=0,high=1)
+                else:
+                    self.prior_dict[f'ellip_{i}'] = dist.Uniform(0,1)
+        elif theta_exists:
+            for i in range(len(self.catalog)):
+                self.prior_dict[f'x0_{i}'] = dist.Normal(self.catalog['X'][i],self.pos_sigma)
+                self.prior_dict[f'y0_{i}'] = dist.Normal(self.catalog['Y'][i],self.pos_sigma)
+                self.prior_dict[f'log_flux_{i}'] = dist.Normal(self.catalog['LOGFLUX'][i],self.logflux_sigma)
+                self.prior_dict[f'r_eff_{i}'] = dist.TruncatedNormal(self.catalog['R'][i], self.rpix_sigma,low=1) 
+                self.prior_dict[f'ellip_{i}'] = dist.Uniform(0,1)
+                if self.catalog['THETA'][i] != -99.0:
+                    self.prior_dict[f'theta_{i}'] = dist.Normal(self.catalog['THETA'][i],self.theta_sigma)
+                else:
+                    self.prior_dict[f'theta_{i}'] = dist.Uniform(0,jnp.pi)
+        elif ellip_exists: 
+            for i in range(len(self.catalog)):
+                self.prior_dict[f'x0_{i}'] = dist.Normal(self.catalog['X'][i],self.pos_sigma)
+                self.prior_dict[f'y0_{i}'] = dist.Normal(self.catalog['Y'][i],self.pos_sigma)
+                self.prior_dict[f'log_flux_{i}'] = dist.Normal(self.catalog['LOGFLUX'][i],self.logflux_sigma)
+                self.prior_dict[f'r_eff_{i}'] = dist.TruncatedNormal(self.catalog['R'][i], self.rpix_sigma,low=1) 
+                self.prior_dict[f'theta_{i}'] = dist.Uniform(0,jnp.pi)
+                if self.catalog['ELLIP'][i] != -99.0:
+                    self.prior_dict[f'ellip_{i}'] = dist.TruncatedNormal(self.catalog['ELLIP'][i],self.ellip_sigma,low=0,high=1)
+                else:
+                    self.prior_dict[f'ellip_{i}'] = dist.Uniform(0,1)
+        else:
+            for i in range(len(self.catalog)):
+                self.prior_dict[f'x0_{i}'] = dist.Normal(self.catalog['X'][i],self.pos_sigma)
+                self.prior_dict[f'y0_{i}'] = dist.Normal(self.catalog['Y'][i],self.pos_sigma)
+                self.prior_dict[f'log_flux_{i}'] = dist.Normal(self.catalog['LOGFLUX'][i],self.logflux_sigma)
+                self.prior_dict[f'r_eff_{i}'] = dist.TruncatedNormal(self.catalog['R'][i], self.rpix_sigma,low=1) 
+                self.prior_dict[f'ellip_{i}'] = dist.Uniform(0,1)
+                self.prior_dict[f'theta_{i}'] = dist.Uniform(0,jnp.pi)
+        return self.prior_dict
+
+
+
+
 
