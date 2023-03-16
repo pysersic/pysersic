9e2fb2239869e4428122fd5d64862762abfe038f
prappleizer <imad.pasha@yale.edu>
2023-03-16 09:32:29 -0400

    bugfix for return type

--- pysersic/utils/results.py
+++ pysersic/utils/results.py
@@ -293,7 +293,7 @@ class PySersicResults():
         out+= "\colhead{Parameter} & \colhead{\hspace{4.5cm}Value\hspace{.5cm}}}\n"
         out+="\caption{Best Fit Parameters for Pysersic Fit}\n"
         out+="\startdata \n"
-        df = self.retrieve_param_quantiles(which=which,quantiles=quantiles,return_type='pandas')
+        df = self.retrieve_param_quantiles(which=which,quantiles=quantiles,return_dataframe=True)
         if len(df.columns)!=3:
             raise AssertionError('Must Choose 3 quantile positions for +/- calculation')
         for i in df.index:
